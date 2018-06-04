65638c236ff53fe8870e9512a814bd0a4a943af7
Sajti Martin <sajti.martin@gmail.com>
2018-06-04 19:13:29 +0200

    created Idiot and Genius players with names

--- src/base/NotAStaticMain.java
+++ src/base/NotAStaticMain.java
@@ -5,13 +5,16 @@ public class NotAStaticMain{
     public NotAStaticMain(){}
 
     public void RunTheGame(){
-        int nDumb,nGenius;
         Player[] jatekos = new Player[21];
+        GiveEmAName nevetad = new GiveEmAName();
         GettingBasicDatas adatbe = new GettingBasicDatas();
         for(int i=0;i<adatbe.nDumb;i++)
         {
-            jatekos[i]= new Idiot();
+            jatekos[i] = new Idiot(nevetad.names[i]);
         }
-
+        for(int i=0;i<adatbe.nGenius;i++)
+        {
+            jatekos[i+adatbe.nDumb] = new Genius(nevetad.names[i+adatbe.nDumb]);
+        }
     }
 }
