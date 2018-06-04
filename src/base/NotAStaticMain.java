6cd5a41fab08f6904c2c476585122fd45d08aa52
Sajti Martin <sajti.martin@gmail.com>
2018-06-04 18:50:09 +0200

    created user data input

--- src/base/NotAStaticMain.java
+++ src/base/NotAStaticMain.java
@@ -1,4 +1,5 @@
 package base;
+import java.util.Scanner;
 
 public class NotAStaticMain{
     public NotAStaticMain(){}
@@ -6,6 +7,12 @@ public class NotAStaticMain{
     public void RunTheGame(){
         int nDumb,nGenius;
         Player[] jatkos = new Player[40];
+        Scanner be = new Scanner(System.in);
+        System.out.print("Buta jatekosok szama:");
+        nDumb = be.nextInt();
+        System.out.println("\n");
+        System.out.print("Okos jatekosok szama:");
+        nGenius = be.nextInt();
 
     }
 }
