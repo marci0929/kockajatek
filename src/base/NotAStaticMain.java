1c30fc09f267875b75619bbffd21c997bfa88761
Sajti Martin <sajti.martin@gmail.com>
2018-06-14 17:58:51 +0200

    Changed arrays to arraylist

--- src/base/NotAStaticMain.java
+++ src/base/NotAStaticMain.java
@@ -1,24 +1,25 @@
 package base;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class NotAStaticMain{
     public NotAStaticMain(){}
 
     public void RunTheGame(){
-        Player[] jatekos = new Player[21];
+        ArrayList<Player> jatekos = new ArrayList<>();
         GiveEmAName nevetad = new GiveEmAName();
         GettingBasicDatas adatbeeski = new GettingBasicDatas();
         adatbeeski.NumOfPlayers();
         PlayerMaker playerMaker = new PlayerMaker();
         playerMaker.MakeThePlayers(jatekos, nevetad, adatbeeski);
-        
+
         for(int i=0;i<adatbeeski.nDumb;i++)
         {
-            System.out.print(jatekos[i].name+",");
+            System.out.print(jatekos.get(i).name+",");
         }
         for(int i=0;i<adatbeeski.nGenius;i++)
         {
-            System.out.print(jatekos[i+adatbeeski.nDumb].name+",");
+            System.out.print(jatekos.get(i+adatbeeski.nDumb).name+",");
         }
         System.out.print("jatszik.");
     }
